9d1990d7e8164ef2085e9383497821ab7056f718
profyoni <jrobinson@touro.edu>
2017-02-20 01:27:49 -0500

    e

--- HumanizeMultTables.Test/UnitTest1.cs
+++ HumanizeMultTables.Test/UnitTest1.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 
 namespace HumanizeMultTables.Test
 {
-
-    public class UnitTest1
+    public class MultTablesTest
     {
         [Test]
-        public void TestMethod1()
+        public void MultiplierRowWithHeader()
         {
+            MultTables.MultiplierRow(1, 3, 4).Should().Contain("three").And.Contain("four").And.Contain("one");
+        }
+
+        [Test]
+        public void MultiplierRowWithoutHeader()
+        {
+            MultTables.MultiplierRow(1, 3, 4, header: false).Should().Contain("three").And.Contain("four").And.NotContain("one");
         }
     }
 }
--- HumanizeMultTables/Program.cs
+++ HumanizeMultTables/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 namespace HumanizeMultTables
 {
+    public static class MultTables
+    {
+        public static string MultiplierRow(int row, int colStart, int colEnd, bool header = true)
+        {
+            StringBuilder sb = new StringBuilder(header ? $"{row.ToWords(),10} | " : "");
+            for (int i = colStart; i <= colEnd; i++)
+            {
+                sb.Append($"{(row * i).ToWords(),10} | ");
+            }
+            return sb.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            const int GridSize = 5;
+            Console.WriteLine($"{"",13}" + MultTables.MultiplierRow(1, 1, GridSize, header: false));
+            for (int i = 1; i <= GridSize; i++)
+            {
+                Console.WriteLine(new String('_', (GridSize + 1) * 13));
+                Console.WriteLine(MultTables.MultiplierRow(i, 1, GridSize));
+            }
+            Console.ReadLine();
         }
     }
 }
